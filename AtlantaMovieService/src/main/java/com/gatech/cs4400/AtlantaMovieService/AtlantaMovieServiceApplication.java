3b96cab6e4fb543db27806b3505371b9ad23820c
benmelnick <bmelnick3@gatech.edu>
2019-11-09 18:56:22 -0500

    [API] Boiler plate Spring Boot code for Atlanta Movie backend API

---
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/AtlantaMovieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gatech.cs4400.AtlantaMovieService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlantaMovieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlantaMovieServiceApplication.class, args);
+	}
+
+}
---
+++ AtlantaMovieService/src/test/java/com/gatech/cs4400/AtlantaMovieService/AtlantaMovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatech.cs4400.AtlantaMovieService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlantaMovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
