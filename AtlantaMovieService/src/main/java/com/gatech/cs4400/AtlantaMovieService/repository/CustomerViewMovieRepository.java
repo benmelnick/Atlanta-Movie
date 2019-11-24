3ad027f560580f397814c096715ea912b96f6a15
benmelnick <bmelnick3@gatech.edu>
2019-11-23 21:35:21 -0500

    [API] Views repository for performing looks up by credit cards or username and date

--- AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/CustomerViewMovieRepository.java
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/CustomerViewMovieRepository.java
@@ -1,10 +1,19 @@
 package com.gatech.cs4400.AtlantaMovieService.repository;
 
+import com.gatech.cs4400.AtlantaMovieService.entity.CreditCard;
 import com.gatech.cs4400.AtlantaMovieService.entity.CustomerViewMovie;
 import com.gatech.cs4400.AtlantaMovieService.entity.CustomerViewMovieId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface CustomerViewMovieRepository extends JpaRepository<CustomerViewMovie, CustomerViewMovieId> {
+
+    List<CustomerViewMovie> findAllByCustomerViewMovieIdCreditCard(CreditCard card);
+
+    List<CustomerViewMovie> findAllByCustomerViewMovieIdMoviePlayMoviePlayIdPlayDateAndCustomerViewMovieIdCreditCardCustomerUsername(Date playDate, String username);
+
 }
