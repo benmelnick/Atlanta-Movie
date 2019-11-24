656884b2dec498d1caf850cae4a9e50426925cf1
benmelnick <bmelnick3@gatech.edu>
2019-11-23 21:31:27 -0500

    [API] Visit repository for performing lookups using a specification executor

--- AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/VisitRepository.java
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/VisitRepository.java
@@ -2,6 +2,7 @@ package com.gatech.cs4400.AtlantaMovieService.repository;
 
 import com.gatech.cs4400.AtlantaMovieService.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface VisitRepository extends JpaRepository<Visit, Integer> {
+public interface VisitRepository extends JpaRepository<Visit, Integer>, JpaSpecificationExecutor<Visit> {
 }
