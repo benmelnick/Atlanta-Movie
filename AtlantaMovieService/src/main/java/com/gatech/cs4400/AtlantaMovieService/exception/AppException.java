94cad4b89993a100537e7e4fe7fff6e26ec2ab3f
benmelnick <bmelnick3@gatech.edu>
2019-11-23 20:51:53 -0500

    [API] Exceptions and API responses for successful and unsuccessful operations

---
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/exception/AppException.java
@@ -0,0 +1,15 @@
+package com.gatech.cs4400.AtlantaMovieService.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class AppException extends RuntimeException {
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
