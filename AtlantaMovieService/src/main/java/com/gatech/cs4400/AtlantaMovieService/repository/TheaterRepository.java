06e1084c32e83fb6ee8db358e290d97f5b2f0ae3
benmelnick <bmelnick3@gatech.edu>
2019-11-23 21:21:45 -0500

    [API] Theater repository to lookup theaters by their parameters

--- AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/TheaterRepository.java
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/TheaterRepository.java
@@ -1,10 +1,24 @@
 package com.gatech.cs4400.AtlantaMovieService.repository;
 
+import com.gatech.cs4400.AtlantaMovieService.entity.Manager;
 import com.gatech.cs4400.AtlantaMovieService.entity.Theater;
 import com.gatech.cs4400.AtlantaMovieService.entity.TheaterId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TheaterRepository extends JpaRepository<Theater, TheaterId> {
+public interface TheaterRepository extends JpaRepository<Theater, TheaterId>, JpaSpecificationExecutor<Theater> {
+
+    Boolean existsByManager(Manager manager);
+
+    Theater findByManager(Manager manager);
+
+    List<Theater> findAllByTheaterIdCompanyName(String companyName);
+
+    Boolean existsByTheaterIdTheaterNameAndTheaterIdCompanyName(String theaterName, String companyName);
+
+    Theater findTheaterByTheaterIdTheaterNameAndTheaterIdCompanyName(String theaterName, String companyName);
 }
