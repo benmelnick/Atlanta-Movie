85075ae92fdd4c5e85c0696a7511f2f26c4b6532
benmelnick <bmelnick3@gatech.edu>
2019-11-23 21:30:33 -0500

    [API] User repository with boolean lookup method

--- AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/UserRepository.java
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.gatech.cs4400.AtlantaMovieService.repository;
 
 import com.gatech.cs4400.AtlantaMovieService.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
+
+    Boolean existsByUsername(String username);
 }
