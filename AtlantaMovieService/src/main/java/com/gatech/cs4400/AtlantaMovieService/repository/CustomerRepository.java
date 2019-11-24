a69164f04f0c48b0c32eec1058545a5cb097b43a
benmelnick <bmelnick3@gatech.edu>
2019-11-23 21:35:47 -0500

    [API] Looking up customers by username

--- AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/CustomerRepository.java
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/CustomerRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+    Boolean existsByUsername(String username);
 }
