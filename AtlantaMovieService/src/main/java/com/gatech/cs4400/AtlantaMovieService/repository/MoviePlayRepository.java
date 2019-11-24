747075218e326e98ae8156d920398f6f94fb1388
benmelnick <bmelnick3@gatech.edu>
2019-11-23 21:17:57 -0500

    [API] Movie play repository with methods for looking for movies playing by: 1) theater name and movie name or 2) theater (existing entity), movie name, and play date

--- AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/MoviePlayRepository.java
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/MoviePlayRepository.java
@@ -1,8 +1,18 @@
 package com.gatech.cs4400.AtlantaMovieService.repository;
 
-import com.gatech.cs4400.AtlantaMovieService.entity.MoviePlay;
-import com.gatech.cs4400.AtlantaMovieService.entity.MoviePlayId;
+import com.gatech.cs4400.AtlantaMovieService.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface MoviePlayRepository extends JpaRepository<MoviePlay, MoviePlayId> {
+import java.util.Date;
+
+public interface MoviePlayRepository extends JpaRepository<MoviePlay, MoviePlayId>, JpaSpecificationExecutor<MoviePlay> {
+
+    Boolean existsByMoviePlayIdTheaterTheaterIdTheaterNameAndMoviePlayIdMovieMovieIdMovieName(String theaterName, String movieName);
+
+    MoviePlay findMoviePlayByMoviePlayIdTheaterAndMoviePlayIdMovieMovieIdMovieNameAndMoviePlayIdPlayDate(Theater theater,
+                                                                                                         String movieName,
+                                                                                                         Date playDate);
+
 }
+
