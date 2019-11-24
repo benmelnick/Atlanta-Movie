26e250944c76406c0c22d06969a5ca99b226a68b
benmelnick <bmelnick3@gatech.edu>
2019-11-23 21:20:50 -0500

    [API] Movie repository for looking up movies in the system created by admins

--- AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/MovieRepository.java
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/repository/MovieRepository.java
@@ -3,8 +3,17 @@ package com.gatech.cs4400.AtlantaMovieService.repository;
 import com.gatech.cs4400.AtlantaMovieService.entity.Movie;
 import com.gatech.cs4400.AtlantaMovieService.entity.MovieId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, MovieId> {
+public interface MovieRepository extends JpaRepository<Movie, MovieId>, JpaSpecificationExecutor<Movie> {
+
+    Boolean existsByMovieId(MovieId movieId);
+
+    Boolean existsByMovieIdMovieName(String movieName);
+
+    Movie findByMovieIdMovieName(String movieName);
+
 }
