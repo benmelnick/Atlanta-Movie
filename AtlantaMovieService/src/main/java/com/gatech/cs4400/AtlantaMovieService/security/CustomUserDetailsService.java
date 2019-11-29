1c5ce21b66389b9a66116cc2aa91639255587068
benmelnick <bmelnick3@gatech.edu>
2019-11-29 17:29:18 -0500

    [API] Pending users may not login either, so included a check that a user who attempts to login is explicitly Approved

--- AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/security/CustomUserDetailsService.java
+++ AtlantaMovieService/src/main/java/com/gatech/cs4400/AtlantaMovieService/security/CustomUserDetailsService.java
@@ -3,14 +3,12 @@ package com.gatech.cs4400.AtlantaMovieService.security;
 import com.gatech.cs4400.AtlantaMovieService.entity.User;
 import com.gatech.cs4400.AtlantaMovieService.exception.DeclinedUserException;
 import com.gatech.cs4400.AtlantaMovieService.repository.UserRepository;
-import com.sun.deploy.net.HttpResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.transaction.Transactional;
 
@@ -30,8 +28,8 @@ public class CustomUserDetailsService implements UserDetailsService {
                         new UsernameNotFoundException("User not found with username : " + username)
                 );
 
-        if (user.getStatus().equals("Declined")) {
-            throw new DeclinedUserException("User is declined with username: " + username);
+        if (!user.getStatus().equals("Approved")) {
+            throw new DeclinedUserException("User is not approved with username: " + username);
         }
 
         return UserPrincipal.create(user);
